e63a5d816df68a8e2022bcf6aa9791bdac923c4c
nailaalfatin <nailaalfatin@gmail.com>
2024-05-14 14:01:12 +0700

    benerin

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Marquee
 const marqueeOne = document.querySelector('.marqueeOne');
 
 marqueeOne.addEventListener('mouseenter', () => {
@@ -17,3 +18,28 @@ marqueeTwo.addEventListener('mouseenter', () => {
 marqueeTwo.addEventListener('mouseleave', () => {
     marqueeTwo.style.animationPlayState = 'running';
 });
+
+//motorbike animation
+// Ambil elemen yang akan dianimasikan
+const element = document.querySelector('.animate-on-scroll');
+
+// Fungsi untuk memulai animasi saat elemen masuk ke dalam jangkauan scroll
+function startAnimation() {
+  element.classList.add('animated'); // Tambahkan kelas untuk memulai animasi
+}
+
+// Fungsi untuk mendeteksi peristiwa scroll
+function handleScroll() {
+  // Ambil posisi elemen dan posisi scroll
+  const elementPosition = element.getBoundingClientRect().top;
+  const scrollPosition = window.innerHeight;
+
+  // Jika posisi elemen kurang dari atau sama dengan posisi scroll
+  // Mulai animasi
+  if (elementPosition <= scrollPosition) {
+    startAnimation();
+  }
+}
+
+// Panggil handleScroll saat terjadi peristiwa scroll
+window.addEventListener('scroll', handleScroll);
