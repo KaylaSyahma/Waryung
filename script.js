77a2183bfc8e80eb121be2778d7f9e1eff1129ea
KaylaSyahma <ksyahma22@gmail.com>
2024-05-15 20:02:52 +0700

    benerin slider

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const marqueeOne = document.querySelector('.marqueeOne');
+const marqueeTwo = document.querySelector('.marqueeTwo');
 
 marqueeOne.addEventListener('mouseenter', () => {
     marqueeOne.style.animationPlayState = 'paused';
@@ -8,12 +9,26 @@ marqueeOne.addEventListener('mouseleave', () => {
     marqueeOne.style.animationPlayState = 'running';
 });
 
-const marqueeTwo = document.querySelector('.marqueeTwo');
-
 marqueeTwo.addEventListener('mouseenter', () => {
     marqueeTwo.style.animationPlayState = 'paused';
 });
 
 marqueeTwo.addEventListener('mouseleave', () => {
     marqueeTwo.style.animationPlayState = 'running';
+});
+
+// script button slider
+document.addEventListener('DOMContentLoaded', function () {
+    const btnRight = document.getElementById('btn-right');
+    const btnLeft = document.getElementById('btn-left');
+    const cardContainer = document.getElementById('jajanan-card');
+    const scrollDistance = 340; // Jarak yang ingin digulirkan setiap kali tombol diklik
+
+    btnRight.addEventListener('click', function () {
+        cardContainer.scrollBy({ left: scrollDistance, behavior: 'smooth' });
+    });
+
+    btnLeft.addEventListener('click', function () {
+        cardContainer.scrollBy({ left: -scrollDistance, behavior: 'smooth' });
+    });
 });
